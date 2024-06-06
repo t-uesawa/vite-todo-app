fb5a1d1989e3b45aa4da76c0b1ce889cac455d0a
t-uesawa <t.uesawa.works@gmail.com>
2024-06-06 10:14:59 +0900

    github pages 対応変更

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), VitePWA({
     manifest: {
+      id: '/vite-todo-app/',
       // ユーザーに通常表示されるアプリ名
       name: 'Todo App (PWA)',
       // name を表示するのに十分なスペースがない場合に表示されるアプリ名
